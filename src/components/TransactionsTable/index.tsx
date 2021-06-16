a99f832eff95626d5f1534eeb2b8798a72f83c24
Rômulo Ferreira Fraga <romulo.fraga.dev@gmail.com>
2021-06-16 20:58:11 -0300

    Connection with fake API

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,31 +1,39 @@
+import { useEffect } from 'react';
 import { Container } from './styles';
 
-export function TransactionsTable () {
+import { api } from '../../services/api';
+
+export function TransactionsTable() {
+  useEffect(() => {
+    api.get('transactions')
+      .then(response => console.log(response.data))
+  }, [])
+
   return (
     <Container>
       <table>
-       <thead>
-         <tr>
-           <th>Title</th>
-           <th>Value</th>
-           <th>Category</th>
-           <th>Date</th>
-         </tr>
-       </thead>
-       <tbody>
-         <tr>
-           <td>Desenvolvimento de website</td>
-           <td className="deposit">R$ 12.000,00</td>
-           <td>Desenvolvimento</td>
-           <td>20/10/2021</td>
-         </tr>
-         <tr>
-           <td>Aluguel</td>
-           <td className="withdraw">- R$ 1.000,00</td>
-           <td>Casa</td>
-           <td>20/10/2021</td>
-         </tr>
-       </tbody>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Value</th>
+            <th>Category</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Website development</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Development</td>
+            <td>20/10/2021</td>
+          </tr>
+          <tr>
+            <td>Rent</td>
+            <td className="withdraw">- R$ 1.000,00</td>
+            <td>Casa</td>
+            <td>20/10/2021</td>
+          </tr>
+        </tbody>
       </table>
     </Container>
   )
