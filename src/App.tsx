d32dd8df96372dd59113287298ac3d8e92098d5e
Rômulo Ferreira Fraga <romulo.fraga.dev@gmail.com>
2021-06-16 21:24:08 -0300

    Implementação de modal com React-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
+import Modal from 'react-modal';
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
+import { useState } from 'react';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-    <Header/>
-    <Dashboard />
-    <GlobalStyle />
+      <Header
+        onOpenNewTransactionModal={handleOpenNewTransactionModal}
+      />
+      <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      >
+        <h2>Register Transaction</h2>
+      </Modal>
+      <GlobalStyle />
     </>
   );
 }
