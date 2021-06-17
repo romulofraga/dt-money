11302130dbe5f0fe813cde2c494b213749cb5b8c
Rômulo Ferreira Fraga <romulo.fraga.dev@gmail.com>
2021-06-16 22:37:30 -0300

    estrutura do modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import Modal from 'react-modal';
 import { GlobalStyle } from './styles/global';
-import { Header } from './components/Header'
-import { Dashboard } from './components/Dashboard'
+import { Header } from './components/Header';
+import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { useState } from 'react';
 
-Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -19,17 +18,9 @@ export function App() {
 
   return (
     <>
-      <Header
-        onOpenNewTransactionModal={handleOpenNewTransactionModal}
-      />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-
-      <Modal
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseTransactionModal}
-      >
-        <h2>Register Transaction</h2>
-      </Modal>
+     <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseTransactionModal}/>
       <GlobalStyle />
     </>
   );
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,24 @@
+import Modal from 'react-modal';
+import { Container } from './styles';
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({isOpen, onRequestClose }:NewTransactionModalProps) {
+
+  Modal.setAppElement('#root');
+
+  return (
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose} overlayClassName="react-modal-overlay" className="react-modal-content">
+      <Container>
+        <h2>Register Transaction</h2>
+        <input placeholder="Title" />
+        <input type="number" placeholder="Value" />
+        <input placeholder="Category" />
+        <button type="submit">Register</button>
+      </Container>
+    </Modal>
+  )
+}
