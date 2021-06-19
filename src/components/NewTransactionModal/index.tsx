a927c98987b86353b5f58a8ac2c6c20db6e7654f
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2021-06-18 23:34:10 -0300

    TransactionTypeContainer

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,23 +1,51 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
-export function NewTransactionModal({isOpen, onRequestClose }:NewTransactionModalProps) {
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
 
   Modal.setAppElement('#root');
 
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose} overlayClassName="react-modal-overlay" className="react-modal-content">
-      <button type="button" onClick={onRequestClose} className="react-modal-close" ><img src={closeImg} alt="Close Modal" /></button>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content">
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Close Modal" />
+      </button>
       <Container>
         <h2>Register Transaction</h2>
         <input placeholder="Title" />
         <input type="number" placeholder="Value" />
+        <TransactionTypeContainer>
+        <button
+          type="button"
+          >
+            <img src={outcomeImg} alt="Outcome" />
+            <span>Outcome</span>
+          </button>
+          
+          <button
+          type="button"
+          >
+            <img src={incomeImg} alt="Income" />
+            <span>Income</span>
+          </button>
+          
+        </TransactionTypeContainer>
         <input placeholder="Category" />
         <button type="submit">Register</button>
       </Container>
